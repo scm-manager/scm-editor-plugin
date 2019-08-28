743bff6c68c135c4f6cba5099a8e2eacb424952e
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-08-28 11:21:26 +0200

    add hover effect to downloadIcon

--- src/main/js/FileDownloadIcon.js
+++ src/main/js/FileDownloadIcon.js
@@ -1,22 +1,34 @@
 //@flow
 import React from "react";
+import injectSheet from "react-jss";
+import classNames from "classnames";
+
+const styles = {
+  hover: {
+    "&:hover": {
+      color: "#363636"
+    }
+  }
+};
 
 type Props = {
   file: any,
+  //context props
+  classes: any
 };
 
 class FileDownloadIcon extends React.Component<Props> {
 
   render() {
-    const { file } = this.props;
+    const {file, classes} = this.props;
     return (
       <>
         <a href={file._links.self.href} download={file.name}>
-          <i className={`fas has-text-link fa-download`}/>
+          <i className={classNames(classes.hover, `fas fa-download`)}/>
         </a>
       </>
     )
   }
 }
 
-export default FileDownloadIcon;
+export default injectSheet(styles)(FileDownloadIcon);
