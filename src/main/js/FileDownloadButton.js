c98874af0c9b16ff13c6dcffdece159c3c0375b6
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-08-28 09:39:41 +0200

    add FileDownloadButton + translations and bind to extensionPoint

---
+++ src/main/js/FileDownloadButton.js
@@ -0,0 +1,43 @@
+// @flow
+import React from "react";
+import {translate} from "react-i18next";
+import injectSheet from "react-jss";
+import classNames from "classnames";
+
+const styles = {
+  button: {
+    height: "40px",
+    width: "50px",
+    "&:hover": {
+      color: "#33b2e8"
+    }
+  }
+};
+
+type Props = {
+  file: any,
+  // context props
+  classes: any,
+  t: string => string
+};
+
+class FileDownloadButton extends React.Component<Props> {
+
+  render() {
+    const {file, classes, t} = this.props;
+    return (
+      <>
+        <a
+          title={t("scm-editor-plugin.download.tooltip")}
+          className={classNames(classes.button, "button")}
+          href={file._links.self.href}
+          download={file.name}
+        >
+          <i className="fas fa-download"/>
+        </a>
+      </>
+    )
+  }
+}
+
+export default injectSheet(styles)(translate("plugins")(FileDownloadButton));
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { binder } from "@scm-manager/ui-extensions";
 import FileDownloadIcon from "./FileDownloadIcon";
+import FileDownloadButton from "./FileDownloadButton";
 
 binder.bind("sourceView.right", FileDownloadIcon);
-binder.bind("fileView.topRight", FileDownloadIcon);
+binder.bind("fileView.actionbar.right", FileDownloadButton);
