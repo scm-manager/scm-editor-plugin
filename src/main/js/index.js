6fddfd8f7f087d1b74c9cd4becbea7615a9a5366
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-08-28 11:09:22 +0200

    add FileUploadButton + translations and bind to extensionPoint

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import { binder } from "@scm-manager/ui-extensions";
 import FileDownloadIcon from "./FileDownloadIcon";
 import FileDownloadButton from "./FileDownloadButton";
+import UploadFileButton from "./FileUploadButton";
 
 binder.bind("repos.sources.tree.row.right", FileDownloadIcon);
 binder.bind("repos.sources.content.actionbar", FileDownloadButton);
+binder.bind("repos.sources.actionbar", UploadFileButton);
