29a2c3afc79a350a2370e09f422498ff340cdbf6
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-08-30 14:33:33 +0200

    enable multi file upload / add fileUploadTable

---
+++ src/main/js/Upload/FileUploadTable.js
@@ -0,0 +1,66 @@
+//@flow
+import React from "react";
+import {translate} from "react-i18next";
+
+
+type Props = {
+  files: File[],
+  removeFile: any => void,
+  //context props
+  t: string => string
+};
+
+class FileUploadTable extends React.Component<Props> {
+
+  removeEntry = (file) => {
+    this.props.removeFileEntry(file);
+  };
+
+  humanFileSize(bytes) {
+    let thresh = 1024;
+    if (Math.abs(bytes) < thresh) {
+      return bytes + ' B';
+    }
+    let units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    let u = -1;
+    do {
+      bytes /= thresh;
+      ++u;
+    } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1) + ' ' + units[u];
+  }
+
+  render() {
+    const {t, files} = this.props;
+
+    return (
+      <table className="card-table table is-hoverable is-fullwidth">
+        <thead>
+        <tr>
+          <th>{t("scm-editor-plugin.upload.file.name")}</th>
+          <th>{t("scm-editor-plugin.upload.file.type")}</th>
+          <th>{t("scm-editor-plugin.upload.file.size")}</th>
+        </tr>
+        </thead>
+        <tbody>
+        {files.map((file) => {
+          return (
+            <tr>
+              <td>{file.name}</td>
+              <td>{file.type}</td>
+              <td>{this.humanFileSize(file.size)}</td>
+              <td>
+                <a onClick={() => this.removeEntry(file)}>
+                  <i className="fas fa-trash-alt"/>
+                </a>
+              </td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default translate("plugins")(FileUploadTable);
