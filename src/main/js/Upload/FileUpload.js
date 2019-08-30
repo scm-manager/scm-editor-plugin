16a39a78f4183185ad83b6165b22e413a3725c82
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-08-30 09:40:44 +0200

    collect userData from redux

--- src/main/js/Upload/FileUpload.js
+++ src/main/js/Upload/FileUpload.js
@@ -1,14 +1,19 @@
 // @flow
 import React from "react";
 import {translate} from "react-i18next";
-import {Subtitle} from "@scm-manager/ui-components"
+import {Subtitle, ButtonGroup, Button} from "@scm-manager/ui-components";
+import {File, Me} from "@scm-manager/ui-types";
 import FileUploadDropzone from "./FileUploadDropzone";
 import FileUploadPath from "./FileUploadPath";
 import {withRouter} from "react-router-dom";
 import CommitMessage from "../CommitMessage";
+import {compose} from "redux";
+import {connect} from "react-redux";
+import {apiClient} from "@scm-manager/ui-components";
 
 
 type Props = {
+  me?: Me,
   //context props
   t: string => string,
   match: any,
@@ -16,7 +21,8 @@ type Props = {
 };
 
 type State = {
-  file?: File
+  files?: File[],
+  commitMessage?: any
 };
 
 class FileUpload extends React.Component<Props, State> {
@@ -31,18 +37,54 @@ class FileUpload extends React.Component<Props, State> {
     this.setState({file});
   };
 
+  commitFile(files, branch, commitMessage) {
+    // apiClient.postBinary("url", );
+  };
+
   render() {
-    const {t} = this.props;
+    const {t, me} = this.props;
     const filePath = this.props.match.params.path;
+    const sourcesLink = this.props.location.pathname.split("upload")[0];
     return (
       <>
         <Subtitle subtitle={t("scm-editor-plugin.upload.title")}/>
         <FileUploadPath path={filePath}/>
         <FileUploadDropzone fileHandler={this.handleFile}/>
-        <CommitMessage/>
+        <br/><br/>
+        <CommitMessage me={me}/>
+        <br/>
+        <div className={"level"}>
+          <div className={"level-left"}/>
+          <div className={"level-right"}>
+            <ButtonGroup>
+              <Button
+                label={t("scm-editor-plugin.upload.button.abort")}
+                link={sourcesLink}
+              />
+              <Button
+                label={t("scm-editor-plugin.upload.button.commit")}
+                color={"primary"}
+                action={this.commitFile(this.state.files, "test", this.state.commitMessage)}
+              />
+            </ButtonGroup>
+          </div>
+        </div>
       </>
     )
   }
 }
 
-export default withRouter(translate("plugins")(FileUpload));
+const mapStateToProps = (state) => {
+  const {auth} = state;
+  const me = auth.me;
+
+  return {
+    me
+  };
+};
+
+export default compose(
+  translate("plugins"),
+  withRouter,
+  connect(mapStateToProps)
+)(FileUpload);
