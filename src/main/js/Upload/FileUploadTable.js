0ce80092b448c18addfdc9c986ca7b6aedc4f792
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-09-05 08:33:59 +0200

    remove color codes from uploadButton / small fixes after review

--- src/main/js/Upload/FileUploadTable.js
+++ src/main/js/Upload/FileUploadTable.js
@@ -3,7 +3,7 @@ import React from "react";
 import {translate} from "react-i18next";
 import injectSheet from "react-jss";
 import classNames from "classnames";
-import {Subtitle} from "@scm-manager/ui-components";
+import {FileSize, Subtitle} from "@scm-manager/ui-components";
 
 const styles = {
   nameColumn: {
@@ -33,20 +33,6 @@ class FileUploadTable extends React.Component<Props> {
     this.props.removeFileEntry(file);
   };
 
-  humanFileSize(bytes) {
-    let thresh = 1000;
-    if (Math.abs(bytes) < thresh) {
-      return bytes + " B";
-    }
-    let units = ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-    let u = -1;
-    do {
-      bytes /= thresh;
-      ++u;
-    } while (Math.abs(bytes) >= thresh && u < units.length - 1);
-    return bytes.toFixed(1) + " " + units[u];
-  }
-
   render() {
     const {t, files, classes, disabled} = this.props;
 
@@ -66,8 +52,8 @@ class FileUploadTable extends React.Component<Props> {
               <th className={classes.nameColumn}>
                 {t("scm-editor-plugin.upload.file.name")}
               </th>
-              <th>{t("scm-editor-plugin.upload.file.type")}</th>
-              <th>{t("scm-editor-plugin.upload.file.size")}</th>
+              <th className="is-hidden-mobile">{t("scm-editor-plugin.upload.file.type")}</th>
+              <th className="is-hidden-mobile">{t("scm-editor-plugin.upload.file.size")}</th>
             </tr>
           </thead>
           <tbody>
@@ -77,8 +63,10 @@ class FileUploadTable extends React.Component<Props> {
                   <td className={classNames(classes.nameColumn)}>
                     {file.name}
                   </td>
-                  <td>{file.type}</td>
-                  <td>{this.humanFileSize(file.size)}</td>
+                  <td className="is-hidden-mobile">{file.type}</td>
+                  <td className="is-hidden-mobile">
+                    <FileSize bytes={file.size}/>
+                  </td>
                   <td>
                     <a onClick={() => !disabled && this.removeEntry(file)}>
                       <i className="fas fa-trash-alt" />
