03f703ece33a83507537c304f600647ecfba262a
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-09-09 15:26:50 +0200

    add route for edit page

--- src/main/js/EditorNavigation.js
+++ src/main/js/EditorNavigation.js
@@ -1,8 +1,9 @@
 //@flow
 import React from "react";
-import { Route } from "react-router-dom";
+import {Route} from "react-router-dom";
 import FileUpload from "./Upload/FileUpload";
-import type { Repository } from "@scm-manager/ui-types";
+import type {Repository} from "@scm-manager/ui-types";
+import FileEdit from "./Edit/FileEdit";
 
 type Props = {
   url: any,
@@ -14,10 +15,16 @@ class EditorNavigation extends React.Component<Props> {
     const { url, repository } = this.props;
 
     return (
-      <Route
-        path={`${this.props.url}/upload/:path*`}
-        render={() => <FileUpload url={url} repository={repository} />}
-      />
+      <>
+        <Route
+          path={`${this.props.url}/upload/:path*`}
+          render={() => <FileUpload url={url} repository={repository}/>}
+        />
+        <Route
+          path={`${this.props.url}/edit/:path*`}
+          render={() => <FileEdit url={url} repository={repository}/>}
+        />
+      </>
     );
   }
 }
