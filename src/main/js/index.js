1c05bd1b20333ceece9dfd1802e0ba8caa4c4d35
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-09-10 16:37:14 +0200

    add create button / wrap sources actionbar button in new component

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -3,13 +3,13 @@ import {binder} from "@scm-manager/ui-extensions";
 import FileDownloadIcon from "./Download/FileDownloadIcon";
 import FileDeleteButton from "./Delete/FileDeleteButton";
 import FileDownloadButton from "./Download/FileDownloadButton";
-import FileUploadButton from "./Upload/FileUploadButton";
 import EditorNavigation from "./EditorNavigation";
 import FileEditButton from "./Edit/FileEditButton";
+import SourcesActionbar from "./SourcesActionbar";
 
 binder.bind("repos.sources.tree.row.right", FileDownloadIcon);
 binder.bind("repos.sources.content.actionbar", FileDeleteButton);
 binder.bind("repos.sources.content.actionbar", FileEditButton);
 binder.bind("repos.sources.content.actionbar", FileDownloadButton);
-binder.bind("repos.sources.actionbar", FileUploadButton);
+binder.bind("repos.sources.actionbar", SourcesActionbar);
 binder.bind("repository.route", EditorNavigation);
