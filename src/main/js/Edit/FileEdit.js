3a272ec49dc49e437e1aa99d55d5498a19787876
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-09-10 11:43:28 +0200

    implement file editing and upload on commit / fix textarea layout

--- src/main/js/Edit/FileEdit.js
+++ src/main/js/Edit/FileEdit.js
@@ -2,21 +2,35 @@
 import React from "react";
 import {translate} from "react-i18next";
 import injectSheet from "react-jss";
-import type {File, Repository} from "@scm-manager/ui-types";
+import type {File, Me, Repository} from "@scm-manager/ui-types";
 import {withRouter} from "react-router-dom";
 import Subtitle from "@scm-manager/ui-components/src/layout/Subtitle";
 import FilePath from "../FilePath";
-import {apiClient, Loading, Textarea} from "@scm-manager/ui-components";
+import {apiClient, Button, ButtonGroup, ErrorNotification, Loading, Textarea} from "@scm-manager/ui-components";
 import queryString from "query-string";
+import {compose} from "redux";
+import {connect} from "react-redux";
+import CommitMessage from "../CommitMessage";
 
 const styles = {
   editor: {
-    minHeight: "20rem"
+    marginBottom: "2rem",
+    "& div": {
+      "& div": {
+        "& textarea": {
+          "&:not([rows])": {
+            minHeight: "30rem",
+            maxHeight: "100rem"
+          }
+        }
+      }
+    }
   }
 };
 
 type Props = {
   repository: Repository,
+  me: Me,
 
   //context props
   t: string => string,
@@ -32,7 +46,9 @@ type State = {
   path: string,
   revision: string,
   error: Error,
-  loading: boolean
+  initialLoading: boolean,
+  loading: boolean,
+  commitMessage: string
 };
 
 class FileEdit extends React.Component<Props, State> {
@@ -40,7 +56,8 @@ class FileEdit extends React.Component<Props, State> {
     super(props);
 
     this.state = {
-      loading: true,
+      initialLoading: false,
+      loading: false,
       pathWithFilename: this.props.match.params.path,
       revision: queryString.parse(this.props.location.search, {
         ignoreQueryPrefix: true
@@ -52,20 +69,7 @@ class FileEdit extends React.Component<Props, State> {
 
   componentDidMount() {
     this.fetchFile();
-  };
-
-  componentDidUpdate() {
-    const {file, loading, content} = this.state;
-
-    if (loading && file && content) {
-      const {file, loading, path} = this.state;
-      const parentDirPath = this.state.pathWithFilename.replace(file.name, "");
-
-      !path && this.setState({path: parentDirPath});
-      loading && this.setState({loading: false});
-      this.setState({loading: false});
-    }
-  };
+  }
 
   fetchFile = () => {
     apiClient
@@ -81,7 +85,16 @@ class FileEdit extends React.Component<Props, State> {
       .get(this.state.file._links.self.href)
       .then(response => response.text())
       .then(content => this.setState({content}))
+      .then(() => this.afterLoading())
       .catch(error => this.setState({error}));
+  };
+
+  afterLoading = () => {
+    const {file, initialLoading, path, pathWithFilename} = this.state;
+    const parentDirPath = pathWithFilename.replace(file.name, "");
+
+    !path && this.setState({path: parentDirPath});
+    initialLoading && this.setState({initialLoading: false});
   };
 
   createFileUrl = () => {
@@ -100,11 +113,11 @@ class FileEdit extends React.Component<Props, State> {
     }
   };
 
-  changePath = (path: string) => {
+  changePath = path => {
     this.setState({path});
   };
 
-  changeFileName = (fileName: string) => {
+  changeFileName = fileName => {
     const {file} = this.state;
     this.setState({...file, file: {name: fileName}});
   };
@@ -113,11 +126,53 @@ class FileEdit extends React.Component<Props, State> {
     this.setState({content});
   };
 
-  render() {
-    const {t, classes} = this.props;
-    const {path, file, content, loading} = this.state;
+  changeCommitMessage = commitMessage => {
+    this.setState({commitMessage});
+  };
 
-    if (loading) {
+  handleError = error => {
+    this.setState({error});
+  };
+
+  commitFile = () => {
+    const {history, repository} = this.props;
+    const {file, commitMessage, path, revision, content} = this.state;
+
+    if (file) {
+      const link = repository._links.modify.href;
+      const blob = new Blob([content], {type: file.type});
+
+      apiClient
+        .postBinary(
+          link.replace("{path}", path) +
+          (revision ? "?branch=" + revision : ""),
+          formdata => {
+            formdata.append("file", blob, file.name);
+            formdata.append(
+              "commit",
+              JSON.stringify({commitMessage, branch: revision})
+            );
+          }
+        )
+        .then(() => history.goBack())
+        .catch(this.handleError);
+    }
+    this.setState({loading: true});
+  };
+
+  render() {
+    const {t, classes, me, location} = this.props;
+    const {
+      path,
+      file,
+      content,
+      initialLoading,
+      loading,
+      error,
+      commitMessage
+    } = this.state;
+
+    if (initialLoading) {
       return <Loading/>;
     }
 
@@ -130,14 +185,54 @@ class FileEdit extends React.Component<Props, State> {
           file={file}
           changeFileName={this.changeFileName}
         />
-        {content && (
-          <div className={classes.editor}>
-            <Textarea value={content} onChange={this.changeFileContent}/>
+        <div className={classes.editor}>
+          <Textarea
+            value={content && content}
+            onChange={this.changeFileContent}
+          />
+        </div>
+        <CommitMessage
+          me={me}
+          commitMessage={commitMessage}
+          onChange={this.changeCommitMessage}
+        />
+        {error && <ErrorNotification error={error}/>}
+        <div className="level">
+          <div className="level-left"/>
+          <div className="level-right">
+            <ButtonGroup>
+              <Button
+                label={t("scm-editor-plugin.button.cancel")}
+                link={location.pathname}
+                disabled={loading}
+              />
+              <Button
+                label={t("scm-editor-plugin.button.commit")}
+                color={"primary"}
+                disabled={!commitMessage || !content}
+                action={() => this.commitFile()}
+                loading={loading}
+              />
+            </ButtonGroup>
           </div>
-        )}
+        </div>
       </>
     );
   }
 }
 
-export default injectSheet(styles)(withRouter(translate("plugins")(FileEdit)));
+const mapStateToProps = state => {
+  const {auth} = state;
+  const me = auth.me;
+
+  return {
+    me
+  };
+};
+
+export default compose(
+  injectSheet(styles),
+  withRouter,
+  connect(mapStateToProps),
+  translate("plugins")
+)(FileEdit);
