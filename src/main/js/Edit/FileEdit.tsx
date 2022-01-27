9a28ad95939e9e9f423a8a848a8a898d53f4adaf
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2022-01-27 15:51:29 +0100

    Fix filenames with hashtag for file editor

--- src/main/js/Edit/FileEdit.tsx
+++ src/main/js/Edit/FileEdit.tsx
@@ -104,7 +104,7 @@ type State = {
   file?: FileWithType;
   content: string;
   initialRevision?: string;
-  path: string;
+  path?: string;
   initialError: Error;
   initialLoading: boolean;
   error: Error;
@@ -180,7 +180,7 @@ class FileEdit extends React.Component<Props, State> {
     const { file, initialLoading, path } = this.state;
     const parentDirPath = this.isEditMode() ? pathWithFilename?.replace(file.name, "") : pathWithFilename;
 
-    if (!path) {
+    if ((!path || !pathWithFilename) && parentDirPath !== pathWithFilename) {
       this.setState({
         path: parentDirPath
       });
@@ -193,7 +193,7 @@ class FileEdit extends React.Component<Props, State> {
     }
   };
 
-  createFileUrl = () =>
+  createFileUrl = (): Promise<string> =>
     new Promise((resolve, reject) => {
       const { repository, revision, path, t } = this.props;
       if (repository._links.sources) {
